915bcaf86038a0d15b4ad02ee3677b748eb31a4b
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-08-28 23:22:43 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
-  patch 'articles/:id', to: 'articles#update'
+    resources :articles, only: [:index, :show, :new, :create, :edit, :update]
 end
